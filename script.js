f7a65cac6202c6b7e16420ac4d77dd9ff4296f75
Brian Simiyu <briansimiyuj@gmail.com>
2026-06-04 09:46:59 +0300

    Global Object

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-const name = 'Brian'
+console.log(global)
 
-console.log(`Hello ${name}!`)
+global.setTimeout(() =>{
+
+    console.log('Ran after 5 seconds')
+
+    clearInterval(interval)
+
+}, 5000)
+
+const interval = setInterval(() =>{
+
+    console.log('Ran after 2 seconds')
+
+}, 2000)
+
+console.log(__dirname)
+console.log(__filename) 
