eb13201ed566dd83f90ff51a9014b5c7197a7777
Brian Simiyu <briansimiyuj@gmail.com>
2026-06-05 09:25:27 +0300

    IP Address and HTTP Protocol

--- script.js
+++
@@ -1,6 +0,0 @@
-import fs from "fs"
-
-const readStream = fs.createReadStream("./files/blog.txt", "utf-8"),
-      writeStream = fs.createWriteStream("./files/blog3.txt")
-
-readStream.pipe(writeStream)
