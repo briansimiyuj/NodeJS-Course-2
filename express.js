2742f0412b957dd6a48f357c51a9fdc1147ce2ef
Brian Simiyu <briansimiyuj@gmail.com>
2026-06-08 14:21:48 +0300

    Refactor views to use EJS templates and update routing in Express

--- express.js
+++ express.js
@@ -6,28 +6,30 @@ const app = express(),
       __fileName = fileURLToPath(import.meta.url),
       __dirName = dirname(__fileName)
 
+app.set("view engine", "ejs")
+
 app.listen(3000)
 
 app.get("/", (req, res) =>{
 
-    res.sendFile(join(__dirName, "views/index.html"))
+    res.render("index")
 
 })
 
 app.get("/about", (req, res) =>{
 
-    res.sendFile(join(__dirName, "views/about.html"))
+    res.render("about")
 
 })
 
-app.get("/about-us", (req, res) =>{
-    
-    res.redirect("/about")
+app.get("/blogs/create", (req, res) =>{
+
+    res.render("create")
 
 })
 
 app.use((req, res) =>{
 
-    res.status(404).sendFile(join(__dirName, "views/404.html"))
+    res.status(404).render("404")
 
 })
