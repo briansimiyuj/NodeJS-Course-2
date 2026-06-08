3307027ed9d1f510aed2d78cf83345fc94124fda
Brian Simiyu <briansimiyuj@gmail.com>
2026-06-08 10:30:01 +0300

    Serving HTML files

--- express.js
+++ express.js
@@ -1,11 +1,21 @@
 import express from "express"
+import { dirname, join } from "path"
+import { fileURLToPath } from "url"
 
-const app = express()
+const app = express(),
+      __fileName = fileURLToPath(import.meta.url),
+      __dirName = dirname(__fileName)
 
 app.listen(3000)
 
 app.get("/", (req, res) =>{
 
-    res.send("<h1>Hello World</h1>")
+    res.sendFile(join(__dirName, "views/index.html"))
+
+})
+
+app.get("/about", (req, res) =>{
+
+    res.sendFile(join(__dirName, "views/about.html"))
 
 })
