d95ba24cd1f3f4523369b80755603227b32af586
Brian Simiyu <briansimiyuj@gmail.com>
2026-06-09 09:07:13 +0300

    JavaScript manipulation in EJS

--- express.js
+++ express.js
@@ -12,24 +12,32 @@ app.listen(3000)
 
 app.get("/", (req, res) =>{
 
-    res.render("index", { title: 'Home' } )
+    const blogs =[
+
+        { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' }
+
+    ]
+
+    res.render("index", { title: 'Home', blogs })
 
 })
 
 app.get("/about", (req, res) =>{
 
-    res.render("about", { title: 'About' } )
+    res.render("about", { title: 'About' })
 
 })
 
 app.get("/blogs/create", (req, res) =>{
 
-    res.render("create", { title: 'Create Blog' } )
+    res.render("create", { title: 'Create Blog' })
 
 })
 
 app.use((req, res) =>{
 
-    res.status(404).render("404", { title: 'Not Found Page'})
+    res.status(404).render("404", { title: 'Not Found Page '})
 
 })
