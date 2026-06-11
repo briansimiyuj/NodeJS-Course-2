c8f221deeb4dc4393dac1043881403ea1b09ec77
Brian Simiyu <briansimiyuj@gmail.com>
2026-06-11 15:30:22 +0300

    Route Parameters

--- express.js
+++ express.js
@@ -74,7 +74,27 @@ app.post("/blogs", (req, res) =>{
         .catch(err =>{
 
             console.log(err)
-            
+
+        })
+    
+})
+
+app.get("/blogs/:id", (req, res) =>{
+
+    const id = req.params.id
+
+    Blog.findById(id)
+
+        .then(result =>{
+
+            res.render("details", { blog: result, title: 'Blog Details' })
+
+        })
+
+        .catch(err =>{
+
+            res.status(404).render("404", { title: 'Not Found Page '})
+
         })
     
 })
